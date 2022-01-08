897f9b9ea3560fb1706c0cb21098d99a45dca9e5
Mr. Leung <ling.home.worker@home.com>
2022-01-08 12:49:16 +0800

    改变显示模式。死彻底了

--- src/boot.s
+++ src/boot.s
@@ -1,7 +1,8 @@
 # Declare constants used for creating a multiboot header.
 .set ALIGN,    1<<0             # align loaded modules on page boundaries
 .set MEMINFO,  1<<1             # provide memory map
-.set FLAGS,    ALIGN | MEMINFO  # this is the Multiboot 'flag' field
+.set VIDINFO,  1<<2
+.set FLAGS,    ALIGN | MEMINFO | VIDINFO # this is the Multiboot 'flag' field
 .set MAGIC,    0x1BADB002       # 'magic number' lets bootloader find the header
 .set CHECKSUM, -(MAGIC + FLAGS) # checksum of above, to prove we are multiboot
 
